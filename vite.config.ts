c0a0e602a0c1b1f515e854c8f868b9fc2e14fdfc
chenqishi <263436967@qq.com>
2025-10-22 20:51:53 +0800

    fix display

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,14 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+    host: '0.0.0.0', // 允许外部访问
+    cors: true, // 启用CORS
+    allowedHosts: [
+      '3b9f40c2.r29.cpolar.top',
+      '33599462.r29.cpolar.top',
+      'localhost',
+      '127.0.0.1'
+    ], // 允许的主机列表
     proxy: {
       '/custom-im': {
         target: 'https://www.lufitglobalselling.com',
